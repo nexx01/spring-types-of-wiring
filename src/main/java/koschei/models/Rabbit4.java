f5d7aae245e526ea19764b24012bed50a0fb042c
nexx01 <nexx01@mail.ru>
2020-08-24 13:15:21 +0300

    Задание: Собрать цепочку до 8го элемента, использовав все вышеперечисленные методы связывания. После выполнения вы должны получить полную фразу. Проверьте своё решение тестом из заготовки.

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,22 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+
+    private Duck5 duck;
+
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + "" + duck;
+    }
+
+    @Autowired
+    public void setOcean(Duck5 duck5) {
+        this.duck = duck5;
     }
 }
